6f2360f788b7f252eaacd38a97d798c2f4559174
inandexhale <inandexhale95@gmail.com>
2022-11-25 20:56:11 +0900

    repository extends querydsl

--- src/main/java/com/guestbook/repository/GuestbookRepository.java
+++ src/main/java/com/guestbook/repository/GuestbookRepository.java
@@ -2,6 +2,7 @@ package com.guestbook.repository;
 
 import com.guestbook.entity.Guestbook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long>, QuerydslPredicateExecutor<Guestbook> {
 }
